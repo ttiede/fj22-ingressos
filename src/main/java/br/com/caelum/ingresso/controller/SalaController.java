099fec4d6f2e1a8d9583933d2c62a1ceb0ae2134
Tiago Tiede <geschopf@gmail.com>
2018-01-13 10:38:36 -0200

    Revert "Controller Sala exibindi valores"
    This reverts commit e0f498fcd924da8818b4d190b8970920e60a6d53.

--- src/main/java/br/com/caelum/ingresso/controller/SalaController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SalaController.java
@@ -1,23 +1,16 @@
 package br.com.caelum.ingresso.controller;
 
-import java.util.Optional;
-
-import javax.validation.Valid;
-
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.model.Sala;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.caelum.ingresso.dao.SalaDao;
-import br.com.caelum.ingresso.dao.SessaoDao;
-import br.com.caelum.ingresso.model.Sala;
+import javax.validation.Valid;
+import java.util.Optional;
 
 /**
  * Created by nando on 03/03/17.
@@ -27,9 +20,7 @@ public class SalaController {
 
     @Autowired
     private SalaDao salaDao;
-    
-    @Autowired
-    private SessaoDao sessaoDao;
+
 
     @GetMapping({"/admin/sala", "/admin/sala/{id}"})
     public ModelAndView form(@PathVariable("id")Optional<Integer> id, Sala sala){
@@ -76,7 +67,6 @@ public class SalaController {
 
         ModelAndView view = new ModelAndView("sessao/lista");
         view.addObject("sala", sala);
-        view.addObject("sessoes", sessaoDao.buscaSessoesDaSala(sala));
 
         return view;
     }
