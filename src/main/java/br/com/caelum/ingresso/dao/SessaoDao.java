a1a8d107c0315b99dc791f53a1c4387e4c0650f9
Tiago Tiede <geschopf@gmail.com>
2018-01-13 09:30:01 -0200

    SessaoDao metodos de Salvar e Listar Sessoes

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -1,10 +1,13 @@
 package br.com.caelum.ingresso.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
 /**
@@ -14,7 +17,10 @@ import br.com.caelum.ingresso.model.Sessao;
 public class SessaoDao {
 	@PersistenceContext
 	private EntityManager manager;
-	private void save( Sessao sessao) {
+	public void save( Sessao sessao) {
 		manager.persist(sessao);
+	}
+	public List<Sessao> buscaSessoesDaSala(Sala sala){
+		return manager.createQuery("SELECT s FROM Sessao s WHERE s.sala = :sala", Sessao.class).setParameter("sala", sala).getResultList();
 	}
 }
