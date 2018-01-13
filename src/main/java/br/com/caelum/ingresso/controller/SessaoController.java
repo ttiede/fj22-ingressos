dc9babc317e7cf42c907d59c27a384eb11ffd69e
Tiago Tiede <geschopf@gmail.com>
2018-01-13 14:40:03 -0200

    Correcao da sessao

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -49,14 +49,13 @@ public class SessaoController {
     @Transactional
     public ModelAndView salva(@Valid SessaoForm form, BindingResult result){
     	if(result.hasErrors()) return form(form.getSalaId(), form);
-    	ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala"+form.getSalaId()+"/sessoes");
+    	ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/"+form.getSalaId()+"/sessoes");
     	Sessao sessao = form.toSessao(salaDao, filmeDao);
     	Sala sala =  salaDao.findOne(form.getSalaId());
     	GerenciadorDeSessao gerenciadorDeSessao = new GerenciadorDeSessao( sessaoDao.buscaSessoesDaSala(sala));
-    	if(gerenciadorDeSessao .cabe(sessao)){
+    	if(gerenciadorDeSessao.cabe(sessao)){
     		sessaoDao.save(sessao);
     	}
-    	
     	return modelAndView;
     }
 }
