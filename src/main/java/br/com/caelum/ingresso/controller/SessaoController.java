75bf48b9423a31b55ee620fd1478e86ef957e409
Tiago Tiede <geschopf@gmail.com>
2018-01-20 15:47:16 -0200

    Carrinho de compras

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
@@ -43,6 +44,9 @@ public class SessaoController {
     
     @Autowired
     private ImdbClient client;
+    
+    @Autowired
+    private Carrinho carrinho;
     
     @GetMapping("/admin/sessao")
     public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form){
@@ -78,6 +82,7 @@ public class SessaoController {
     	Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
         
     	modelAndView.addObject("sessao", sessao);
+    	modelAndView.addObject("carrinho", carrinho);
     	modelAndView.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
     	modelAndView.addObject("tiposDeIngressos", TipoDeIngresso.values());
     	
---
+++ src/main/java/br/com/caelum/ingresso/model/form/CarrinhoForm.java
@@ -0,0 +1,37 @@
+package br.com.caelum.ingresso.model.form;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.caelum.ingresso.dao.LugarDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
+
+/**
+ * Created by Tiago Tiede on 20/01/18.
+ */
+public class CarrinhoForm {
+	private List<Ingresso> ingressos = new ArrayList<>();
+
+	public List<Ingresso> getIngressos(){
+		return ingressos;
+	}
+	
+	public void setIngressos(List<Ingresso> ingresso){
+		this.ingressos = ingresso;
+	}
+	public List<Ingresso> toIngressos(SessaoDao sessaoDao, LugarDao lugarDao){
+		
+		return this.ingressos.stream().map(ingresso ->{
+			Sessao sessao = sessaoDao.fineOne(ingresso.getSessao().getId());
+			Lugar lugar = lugarDao.findOne(ingresso.getLugar().getId());
+			TipoDeIngresso tipoDeIngresso = ingresso.getTipoDeIngresso();
+			return new Ingresso(sessao, tipoDeIngresso, lugar);
+			
+		}).collect(Collectors.toList());
+	}
+}
