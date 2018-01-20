23ab695dd0e91c4d1dbe1f5b7976b7726f8c320c
Tiago Tiede <geschopf@gmail.com>
2018-01-20 16:02:00 -0200

    Carrinho de compra

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,5 +27,13 @@ public class Carrinho {
 
 	public void add(Ingresso ingresso) {
 		this.ingressos.add(ingresso);
+	}
+	
+	public boolean isSelecionado(Lugar lugar){
+		return ingressos.stream().map(Ingresso::getLugar).anyMatch(l -> l.equals(lugar));
+	}
+	
+	public BigDecimal getTotal(){
+		return ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
 	}
 }
